f63951a4af501dfa9900c0aa2ea611c798cb375c
BijanProgrammer <bijaneisapour@gmail.com>
2024-10-13 20:18:10 +0330

    [ADD] json endpoint

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
 import express from "express";
+import fs from "fs/promises";
 
 const app = express();
 const port = 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/clinics", async (req, res) => {
+  const content = await fs.readFile("src/data/clinics.json", {
+    encoding: "utf-8",
+  });
+
+  const clinics = JSON.parse(content);
+
+  res.json(clinics);
 });
 
 app.listen(port, () => {
